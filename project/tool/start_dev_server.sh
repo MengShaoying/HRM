fe25756ff7577007148c74bf392aaa61d99ce2c0
yaoyang <kiki@smarty.so>
2018-11-04 10:51:37 +0800

    修改为 hrm 项目框架

--- project/tool/start_dev_server.sh
+++ project/tool/start_dev_server.sh
@@ -4,9 +4,9 @@ ROOT_DIR="$(cd "$(dirname $0)" && pwd)"/../../
 
 sh $ROOT_DIR/project/tool/dep_build.sh link
 
-sudo docker run --rm -ti -p 80:80 -p 3306:3306 --name mvc_frame \
+sudo docker run --rm -ti -p 80:80 -p 3306:3306 --name hrm \
     -v $ROOT_DIR/../frame:/var/www/frame \
-    -v $ROOT_DIR:/var/www/mvc_frame \
-    -v $ROOT_DIR/project/config/development/nginx/mvc_frame.conf:/etc/nginx/sites-enabled/default \
+    -v $ROOT_DIR:/var/www/hrm \
+    -v $ROOT_DIR/project/config/development/nginx/hrm.conf:/etc/nginx/sites-enabled/default \
     -v $ROOT_DIR/project/config/development/supervisor/queue_worker.conf:/etc/supervisor/conf.d/queue_worker.conf \
 kikiyao/debian_php_dev_env start
